61be66c7acb6637e94e5f397ac7becbfc95c354d
soborobbang <58281350+soborobbang@users.noreply.github.com>
2020-06-09 03:10:59 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,8 @@
-package com.cookandroid.share;
+package com.example.git_test;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,21 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btn1 = findViewById(R.id.btn1);
-        final TextView Tv = (TextView) findViewById(R.id.Tv);
-
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent Sharing_intent = new Intent(Intent.ACTION_SEND);
-                Sharing_intent.setType("text/plain");
-
-                Sharing_intent.putExtra(Intent.EXTRA_TEXT, Tv.getText());
-
-                Intent Sharing = Intent.createChooser(Sharing_intent, "공유하기");
-                startActivity(Sharing);
-            }
-        });
     }
 }
